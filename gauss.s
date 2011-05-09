b014747a8ea94e3182954f021f22d228ddb25113
Arash Rouhani <miffoljud@gmail.com>
2011-05-09 16:45:29 +0200

    Unjerked Jonas

--- gauss.s
+++ gauss.s
@@ -85,11 +85,6 @@ loop_pivot_row_dividing:
 		s.s		$f1, ($t8)			# set [diag]=1 
 		add		$t8, $t8, $t9		# diag+=n+1; where t8 is diag and t9 is n+1		
 		bne		$a0, $t0, loop_outermost		# jump back to outermost loop
-		
-		#######################################################################
-		#			JONAS was here lolol
-		#######################################################################
-		
 		nop
 		s.s		$f1, ($t8)			# set [diag]=1 
 
